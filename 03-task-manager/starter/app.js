dcdd95e47d0e8d1aa54bebd09d67669b32c4e402
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-03-29 22:14:51 -0400

    homework week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require('dotenv').config();
-
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
 //middleware
+app.use(express.static("./public"));
 app.use(express.json());
-
+const errorHandlerMiddleware = require("./middleware/error-handler");
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager App");
-});
-app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
