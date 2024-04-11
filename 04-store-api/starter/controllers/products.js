f65bb899bb8a10d78ae298784db1dc9afe14f2c2
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-04-11 15:44:08 -0400

    sorting functionality added

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,29 +1,36 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "a first wooden table",
-  });
+  const search = "a";
+  const products = await Product.find({}).sort("name price");
   // throw new Error("Throw async errors");
-  res.status(200).json({ products });
-};
-const getAllProductsStaticLiddy = async (req, res) => {
-  const products = await Product.find({
-    company: "liddy",
-  });
-  // throw new Error("Throw async errors");
-  res.status(200).json({ products });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   console.log(req.query);
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? "true" : "false";
   }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log("queryObject:");
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    console.log(sort);
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
