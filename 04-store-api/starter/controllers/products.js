d1987c5e99900bd07f587248d378aa64f3d655e3
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-04-11 19:39:53 -0400

    skip, limit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -43,6 +43,17 @@ const getAllProducts = async (req, res) => {
     const fieldstList = fields.split(",").join(" ");
     result = result.select(fieldstList);
   }
+
+  //skip, limit
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  const fieldstList = fields.split(",").join(" ");
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
