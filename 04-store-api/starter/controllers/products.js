9f9803f95479455824a24ea4e07abbc57e6d62fe
Kremifany <34751960+Kremifany@users.noreply.github.com>
2024-04-16 23:21:19 -0400

    Changes according to review

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,11 @@ const Product = require("../models/product");
 //////////////////////////////////////////////////////////////////STATIC/////////////////////////////////////////////////////////////////////////////
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "a";
   const products = await Product.find({ price: { $gt: 100 } })
     .sort("name")
     .select("name price")
     .limit(10)
-    .skip(1).numericFilters;
+    .skip(1);
   // throw new Error("Throw async errors");
   res.status(200).json({ products, nbHits: products.length });
 };
@@ -18,7 +17,7 @@ const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
-    queryObject.featured = featured === "true" ? "true" : "false";
+    queryObject.featured = featured === "true";
   }
   if (company) {
     queryObject.company = company;
@@ -30,20 +29,40 @@ const getAllProducts = async (req, res) => {
     const operatorMap = {
       ">": "$gt",
       ">=": "$gte",
+      "=": "$eq",
       "<": "$lt",
       "<=": "$lte",
-      "=": "$eq",
     };
-    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-    let filters = numericFilters.replace(
-      regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
-    const options = ["price", "rating"];
-    filters = filters.split(",").forEach((element) => {
-      const [field, operator, value] = element.split("-");
-      if (options.includes(field)) {
-        queryObject[field] = { [operator]: Number(value) }
+
+    const allowedFilterProps = {
+      price: "price",
+      rating: "rating",
+    };
+
+    // first, split the query into filters: [ "price>10", "price<20", "rating=4.5" ],
+    // then iterate over each filter
+    numericFilters.split(",").forEach((filter) => {
+      // sanitize filter by keeping Aa-Zz, 0-9, <, =, >, .
+      const santizedFilter = filter.replace(/[^\w<=>\.]/g, "");
+
+      // find the matching operator
+      const operator = santizedFilter.match(/\b(<|>|>=|=|<=)\b/g);
+
+      // match operator to Mongo string
+      const allowedOperator = operatorMap[operator];
+
+      // split filter via operator: "price<20" becomes ["price", "20"]
+      const [prop, value] = santizedFilter.split(operator);
+
+      // match prop to allowed filter property
+      const allowedFilter = allowedFilterProps[prop];
+
+      if (allowedFilter && allowedOperator && value) {
+        // assign filter to query: { price: { $gt: 10, $lt: 20 }, rating: { $eq: 4.5 } }
+        queryObject[allowedFilter] = {
+          ...queryObject[allowedFilter],
+          [allowedOperator]: parseFloat(value),
+        };
       }
     });
   }
@@ -70,8 +89,8 @@ const getAllProducts = async (req, res) => {
 
   //skip, limit
 
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
 
   const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
